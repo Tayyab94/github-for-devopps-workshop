da1fb2e236bd5abc7383623df8c7bf7ed6569dcb
Tayyab94 <tayyab.bhatti30@gmail.com>
2024-09-10 23:38:11 +0500

    Rock Paper Game v1

--- rock_paper_game.py
+++ rock_paper_game.py
@@ -2,6 +2,12 @@
 import random
 choices=("r","p","s")
 
+# Winning Choice by Dic
+winning_combinations = {
+    "r": "s",  # Rock beats Scissors
+    "s": "p",  # Scissors beat Paper
+    "p": "r",  # Paper beats Rock
+}
 while True:
     user_choice = input("Rock, Paper or scissors? (r/p/s) : ").lower()
 
@@ -9,15 +15,18 @@ while True:
         print("Invalid Choice!")
         continue
     computer_choice= random.choice(choices)
+
     print(f"YOu Choose :{user_choice}")
     print(f"Computer Choose: {computer_choice}")
     if user_choice ==computer_choice:
         print("Tie!")
-    elif (
-        (user_choice=="r" and computer_choice=="s") or 
-        (user_choice=="s" and computer_choice=="p") or 
-        (user_choice=="p" and computer_choice=="r")):
-        print("you Win!")
+    # elif (
+    #     (user_choice=="r" and computer_choice=="s") or 
+    #     (user_choice=="s" and computer_choice=="p") or 
+    #     (user_choice=="p" and computer_choice=="r")):
+    #     print("you Win!")
+    elif winning_combinations[user_choice]== computer_choice:
+        print("You Win!")
     else:
         print("Computer Win!")
 
