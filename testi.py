ccba654ef95c429921459b81c99eb14d91fc8dcf
Tayyab94 <tayyab.bhatti30@gmail.com>
2024-09-10 21:34:26 +0500

    explain inside 1

--- testi.py
+++ testi.py
@@ -1,6 +1,7 @@
 
 def my_function():
-    """ This module provides functions for testing various components.  module includes functions for unit testing, integration testing, and other testing utilities.
+    """ This module provides functions for testing various components.  
+    module includes functions for unit testing, integration testing, and other testing utilities.
 
      Example:
        # Example usage of a testing function
--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 
 
 def user_name():
-    """ This module provides functions for testing various components.  module includes functions for unit testing, integration testing, and other testing utilities.
+    """ This module provides functions for testing various components.  
+    module includes functions for unit testing, integration testing, and other testing utilities.
 
      Example:
        # Example usage of a testing function
