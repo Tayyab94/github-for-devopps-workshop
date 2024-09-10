df65a695bf9fd59ec6a1206aff5d91140275075a
Tayyab94 <tayyab.bhatti30@gmail.com>
2024-09-10 23:52:26 +0500

    Rock Paper Game v1.2

--- rock_paper_game.py
+++ rock_paper_game.py
@@ -1,33 +1,33 @@
-"""this is game Rock - Paper """
-import random
-choices=("r","p","s")
+"""This is the Rock - Paper - Scissors game."""
 
-# Winning Choice by Dic
+import random
+
+choices = ("r", "p", "s")
+
+# Winning combinations dictionary
 winning_combinations = {
     "r": "s",  # Rock beats Scissors
     "s": "p",  # Scissors beat Paper
     "p": "r",  # Paper beats Rock
 }
+
 while True:
-    user_choice = input("Rock, Paper or scissors? (r/p/s) : ").lower()
+    user_choice = input("Rock, Paper, or Scissors? (r/p/s): ").lower()
     if user_choice not in choices:
         print("Invalid Choice!")
         continue
-    computer_choice= random.choice(choices)
-    print(f"YOu Choose :{user_choice}")
-    print(f"Computer Choose: {computer_choice}")
-    if user_choice ==computer_choice:
+
+    computer_choice = random.choice(choices)
+    print(f"You chose: {user_choice}")
+    print(f"Computer chose: {computer_choice}")
+
+    if user_choice == computer_choice:
         print("Tie!")
-    # elif (
-    #     (user_choice=="r" and computer_choice=="s") or 
-    #     (user_choice=="s" and computer_choice=="p") or 
-    #     (user_choice=="p" and computer_choice=="r")):
-    #     print("you Win!")
-    elif winning_combinations[user_choice]== computer_choice:
+    elif winning_combinations[user_choice] == computer_choice:
         print("You Win!")
     else:
-        print("Computer Win!")
+        print("Computer Wins!")
 
-    should_continue= input("continue : (y/n): ").lower()
-    if should_continue=='n':
+    should_continue = input("Continue? (y/n): ").lower()
+    if should_continue == 'n':
         break
